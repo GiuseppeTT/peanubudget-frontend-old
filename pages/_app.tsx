f21147f508549c8fec6e919aa8f2d8614a4e039f
GiuseppeTT <giu.tinti@gmail.com>
2023-02-07 00:53:31 -0800

    Add material UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
+import { Roboto_Flex } from "@next/font/google";
 import type { AppProps } from "next/app";
 
+const roboto_flex = Roboto_Flex({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={roboto_flex.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
