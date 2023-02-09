e2fb3696f2e5a837654e04b7dd8bebdb3cf8dc46
GiuseppeTT <giu.tinti@gmail.com>
2023-02-09 11:06:58 -0800

    Improve GitHub button

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -4,8 +4,6 @@ import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 
-const handleClick = () => window.open(process.env.NEXT_PUBLIC_SOURCE_CODE_LINK);
-
 export default function TopBar() {
   return (
     <AppBar
@@ -16,7 +14,12 @@ export default function TopBar() {
         <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
           PeanuBudget
         </Typography>
-        <IconButton onClick={handleClick} color="inherit" sx={{ padding: 0 }}>
+        <IconButton
+          component="a"
+          href={process.env.NEXT_PUBLIC_SOURCE_CODE_LINK}
+          color="inherit"
+          sx={{ padding: 0 }}
+        >
           <GitHubIcon />
         </IconButton>
       </Toolbar>
